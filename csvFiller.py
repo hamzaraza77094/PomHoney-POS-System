710f344da64db848c990de0ac48795357ae30254
unnamed <adam@ivick.us>
2023-02-28 19:18:06 -0600

    Error Checking

--- csvFiller.py
+++ csvFiller.py
@@ -29,9 +29,10 @@ def randomMeal():
     
     subTotal = 0
     saleContents = ""
-    
-    for i in range(len(number_of_items)):
+    print(number_of_items)
+    for i in range(number_of_items):
         item_dict = categories[choice(range(0,number_of_categories))]
+        print(item_dict)
         if item_dict == 1:
             item_in_entree = choice([0,1])
             
@@ -61,13 +62,20 @@ def randomMeal():
         
         else:
             print("Shit")
-            
+            return
+        
         salesTax = subTotal * 0.0625
         
         return subTotal, salesTax, saleContents
   
 with open('./q1.csv', 'w') as outCSV:
     outCSV.write('salesID, dateOfSale, saleSubTotal, saleTax, saleContents')
+    quarter = 1
+    month = 0
     for i in range(1, 90):
+        if i == 31:
+            month = 2
+        elif month == 59:
+            month = 3
         randoSubTotal, randoSalesTax, randoSalesContent = randomMeal()
-        outCSV.write(f'{i}, {}/{}/2023, {randoSubTotal}, {randoSalesTax}, {randoSalesContent}')
+        outCSV.write(f'{i}, 0{month}/{i}/2023, {randoSubTotal}, {randoSalesTax}, {randoSalesContent}')
