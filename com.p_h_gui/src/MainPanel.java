da95c9d80eb45a99e7200dfd59df42982e47494e
unnamed <adam@ivick.us>
2023-03-06 10:15:54 -0600

    Made Order Cycle siclical(made it loop on itself).

--- com.p_h_gui/src/MainPanel.java
+++ com.p_h_gui/src/MainPanel.java
@@ -1,4 +1,5 @@
 import javax.swing.JPanel;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.CardLayout;
@@ -10,11 +11,9 @@ import java.awt.Dimension;
 
 public class MainPanel extends JPanel{
     
+    // CardLayout for MainPanel
     public CardLayout mainCardLayout;
-    public CardLayout checokCardLayout;
-    // private JPanel subPanel1;
-    // private JPanel subPanel2;
-
+    //Sub Panels to Main Panel
     public JPanel checkoutPanel;
     public JPanel entreesPanel;
     public JPanel extrasPanel;
@@ -25,134 +24,119 @@ public class MainPanel extends JPanel{
         mainCardLayout = new CardLayout();
         setLayout(mainCardLayout);
         setPreferredSize(new Dimension(1100, 700));
+    
+      //Checkout Menu TODO: MOVE TO ANOTHER CLASS TO CLEAN UP CODE
+        JPanel checkoutPanel = new JPanel();
         
-    //Checkout Panel
-        JPanel checkoutPanel = new JPanel(); // Make wrapper panel
-        CardLayout checkCardLayout = new CardLayout();
-        checkoutPanel.setLayout(checkCardLayout); // Make it so the sub panels can be selected and take up the whole space
-        JButton entrees_button = new JButton("Entrees");
-        entrees_button.addActionListener(new ActionListener() {
+        JButton entrees_Button = new JButton("Entrees");
+        entrees_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-                showMainPanel("entreesSubMenu"); //TODO: Give correct options
-                System.out.println("Entrees SubMenu Button was pressed.");
+                showMainPanel("entreesPanel");
             }
         });
-        checkoutPanel.add(entrees_button);
-        JButton extras_button = new JButton("Extras");
-        extras_button.addActionListener(new ActionListener() {
+        
+        JButton extras_Button = new JButton("Extras");
+        extras_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-                showMainPanel("extrasSubMenu"); //TODO: Give correct options
-                System.out.println("Extras SubMenu Button was pressed.");
+                showMainPanel("extrasPanel");
             }
         });
-        checkoutPanel.add(extras_button);
-        JButton drinks_button = new JButton("Drinks");
-        drinks_button.addActionListener(new ActionListener() {
+        
+        JButton drinks_Button = new JButton("Drinks");
+        drinks_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-                showMainPanel("drinksEntreeMenu"); //TODO: Give correct options
-                System.out.println("Drinks SubMenu Button was pressed.");
+                showMainPanel("drinksPanel");
             }
-        });      
-        checkoutPanel.add(drinks_button);
-
-        JLabel temp = new JLabel("This is the Checkout Panel");
-
-        // entreesPanel = createEntreePanel();
-        // extrasPanel = createExtrasPanel();
-        // drinksPanel = createDrinksPanel();
-
-        // checkoutPanel.add(entreesPanel, "entreesSubPanel");
-        // checkoutPanel.add(extrasPanel, "extrasSubPanel");
-        // checkoutPanel.add(drinksPanel, "drinksSubPanel");
-
-        // showCheckoutPanel("extrasSubPanel");
-        checkoutPanel.add(temp);
+        });
+        
+        checkoutPanel.add(entrees_Button);
+        checkoutPanel.add(extras_Button);
+        checkoutPanel.add(drinks_Button);
         add(checkoutPanel, "checkoutPanel");
-    //Entree SubPanel
-        JPanel entreePanel = new JPanel();
-        JButton gyroButton = new JButton("Gyro");
-        gyroButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                showMainPanel("extrasSubMenu"); //TODO: Give correct options
-                System.out.println("Gyro Button was pressed.");
-            }
-        });
-        JButton bowlButton = new JButton("Bowl");
-        bowlButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                showMainPanel("extrasSubMenu"); //TODO: Give correct options
-                System.out.println("Bowl Button was pressed.");
-            }
-        });
-        entreePanel.add(gyroButton);
-        entreePanel.add(bowlButton);
-        add(entreePanel, "entreeSubMenu");
         
-    //Extras SubPanel
+      //Entrees Menu TODO: MOVE TO ANOTHER CLASS TO CLEAN UP CODE
+        JPanel entreesPanel = new JPanel();
+        
+        JButton gyro_Button = new JButton("Gyro");
+        gyro_Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showMainPanel("extrasPanel"); //TODO: ADD CORRECT ACTIONS    
+            }
+        });
+        
+        JButton bowl_Button = new JButton("Bowl");
+        bowl_Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showMainPanel("extrasPanel"); //TODO: ADD CORRECT ACTIONS    
+            }
+        });
+        
+        entreesPanel.add(gyro_Button);
+        entreesPanel.add(bowl_Button);
+        add(entreesPanel, "entreesPanel");
+
+      //Extras Menu TODO: MOVE TO ANOTHER CLASS TO CLEAN UP CODE
         JPanel extrasPanel = new JPanel();
         
-        JButton hum_fria = new JButton("Hummas & Pita");
-        hum_fria.addActionListener(new ActionListener() {
+        JButton dosFalafel_Button = new JButton("2 Falafells");
+        dosFalafel_Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
-                showMainPanel("drinksSubMenu"); //TODO: Give correct options
-                System.out.println("Hummas & Pita was pressed.");
-            }
-        });
-        JButton dos_falafels = new JButton("2 Falafels");
-        dos_falafels.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                showMainPanel("drinksSubMenu"); //TODO: Give correct options
-                System.out.println("2 Falafells was pressed.");
-            }
-        });
-        JButton x_protein = new JButton("Extra Protein");
-        hum_fria.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                showMainPanel("drinksSubMenu"); //TODO: Give correct options
-                System.out.println("Extra Protein was pressed.");
-            }
-        });
-        JButton x_dressing = new JButton("Extra Dressing");
-        x_dressing.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                showMainPanel("drinksSubMenu"); //TODO: Give correct options
-                System.out.println("Extra Dressing was pressed.");
+            public void actionPerformed(ActionEvent e) {
+                showMainPanel("drinksPanel"); //TODO: ADD CORRECT ACTIONS    
             }
         });
         
-        extrasPanel.add(hum_fria);
-        extrasPanel.add(dos_falafels);
-        extrasPanel.add(x_protein);
-        extrasPanel.add(x_dressing);
-        add(extrasPanel, "extrasSubMenu");
-        System.out.print("Extras Panel Created.");
-
-    //Drinks SubPanel
-        JPanel drinksPanel = new JPanel();
-        JButton soft_drink = new JButton("Soft Drink");
-        soft_drink.addActionListener(new ActionListener() {
+        JButton humPita_Button = new JButton("Hummas & Pita");
+        humPita_Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
-                showMainPanel("entreesSubMenu"); //TODO: Give correct options
-                System.out.println("Drinks SubMenu button was pressed.");
+            public void actionPerformed(ActionEvent e) {
+                showMainPanel("drinksPanel"); //TODO: ADD CORRECT ACTIONS    
             }
         });
-        drinksPanel.add(soft_drink);
-        add(drinksPanel, "drinksSubMenu");
-        // checkoutPanel = createCheckoutPanel();
-        showMainPanel("drinksSubMenu");
-        System.out.print("Main Panel Created\n");
-    } // Constructor of Main Panel
+        
+        JButton xProtein_Button = new JButton("Extra Protein");
+        xProtein_Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showMainPanel("drinksPanel"); //TODO: ADD CORRECT ACTIONS    
+            }
+        });
+        
+        JButton xDressing_Button = new JButton("Extra Dressing");
+        xDressing_Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showMainPanel("drinksPanel"); //TODO: ADD CORRECT ACTIONS    
+            }
+        });
+        
+        extrasPanel.add(dosFalafel_Button);
+        extrasPanel.add(humPita_Button);
+        extrasPanel.add(xProtein_Button);
+        extrasPanel.add(xDressing_Button);
+        add(extrasPanel, "extrasPanel");
 
+      //Drinks Menu TODO: MOVE TO ANOTHER CLASS TO CLEAN UP CODE
+        JPanel drinksPanel = new JPanel();
+        
+        JButton drink_Button = new JButton("Soft Drink");
+        drink_Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                showMainPanel("entreesPanel");
+            }
+        });
+        
+        drinksPanel.add(drink_Button);
+        add(drinksPanel, "drinksPanel");
+
+        showMainPanel("checkoutPanel");
+    } // Constructor of Main Panel
 
     public void showMainPanel(String panelName){
         mainCardLayout.show(this, panelName);
