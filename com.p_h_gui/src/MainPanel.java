d7c3cd26190e925539c484c64fc40b0faec7a227
unnamed <adam@ivick.us>
2023-03-06 08:22:47 -0600

    Moving all main panels into one panel

--- com.p_h_gui/src/MainPanel.java
+++ com.p_h_gui/src/MainPanel.java
@@ -27,17 +27,55 @@ public class MainPanel extends JPanel{
         setPreferredSize(new Dimension(1100, 700));
         
         
-        entreesPanel = createEntreePanel();
-        extrasPanel = createExtrasPanel();
-        drinksPanel = createDrinksPanel();
-        checkoutPanel = createCheckoutPanel();
+        JPanel checkoutPanel = new JPanel(); // Make wrapper panel
+        CardLayout checkCardLayout = new CardLayout();
+        checkoutPanel.setLayout(checkCardLayout); // Make it so the sub panels can be selected and take up the whole space
 
-        checkoutPanel.add(entreesPanel, "entreesSubPanel");
-        checkoutPanel.add(extrasPanel, "extrasSubPanel");
-        checkoutPanel.add(drinksPanel, "drinksSubPanel");
+        JLabel temp = new JLabel("This is the Checkout Panel");
+
+        // entreesPanel = createEntreePanel();
+        // extrasPanel = createExtrasPanel();
+        // drinksPanel = createDrinksPanel();
+
+        // checkoutPanel.add(entreesPanel, "entreesSubPanel");
+        // checkoutPanel.add(extrasPanel, "extrasSubPanel");
+        // checkoutPanel.add(drinksPanel, "drinksSubPanel");
+
+        // showCheckoutPanel("extrasSubPanel");
+        checkoutPanel.add(temp);
+        //Entree SubPanel
+        JPanel entreePanel = new JPanel();
+        JButton gyroButton = new JButton();
+        JButton bowlButton = new JButton();
+        entreePanel.add(gyroButton);
+        entreePanel.add(bowlButton);
+        add(entreePanel, "entreeSubMenu");
+        
+        //Extras SubPanel
+        JPanel extrasPanel = new JPanel();
+        JButton hum_fria = new JButton();
+        JButton dos_falafels = new JButton();
+        JButton x_protein = new JButton();
+        JButton x_dressing = new JButton();
+        extrasPanel.add(hum_fria);
+        extrasPanel.add(dos_falafels);
+        extrasPanel.add(x_protein);
+        extrasPanel.add(x_dressing);
+        add(extrasPanel, "extrasSubMenu");
+
+        //Drinks SubPanel
+        JPanel drinksPanel = new JPanel();
+        JButton soft_drink = new JButton();
+        drinksPanel.add(soft_drink);
+        add(drinksPanel, "drinksSubMenu");
+        // checkoutPanel = createCheckoutPanel();
+
+        
 
         add(checkoutPanel, "checkoutPanel");
-        showCheckoutPanel("extrasSubPanel");
+        showMainPanel("extrasSubPanel");
+        // showCheckoutPanel("entreeSubMenu");
+        // showCheckoutPanel("extrasSubPanel");
         // subPanel1 = new JPanel();
         // JLabel tempLabel1 = new JLabel("HOwdy!");
         // JButton switchToPanel2Button = new JButton("Switch to Panel 2");
@@ -75,91 +113,59 @@ public class MainPanel extends JPanel{
     public void showMainPanel(String panelName){
         mainCardLayout.show(this, panelName);
     }
-    public void showCheckoutPanel(String panelName){
-        checokCardLayout.show(this, panelName);
-    }
+    // public void showCheckoutPanel(String panelName){
+    //     checokCardLayout.show(this, panelName);
+    // }
 
-    public JPanel createCheckoutPanel(){
-        //Idea is to have 3 submenus that loop back on eachother.
-        JPanel checkoutPanel = new JPanel(); // Make wrapper panel
-        CardLayout checkCardLayout = new CardLayout();
-        checkoutPanel.setLayout(checkCardLayout); // Make it so the sub panels can be selected and take up the whole space
-
-        JLabel temp = new JLabel("This is the Checkout Panel");
-        checkoutPanel.add(temp);
-        //Entree SubPanel
-        // JPanel entreePanel = new JPanel();
-        // JButton gyroButton = new JButton();
-        // JButton bowlButton = new JButton();
-        // entreePanel.add(gyroButton);
-        // entreePanel.add(bowlButton);
-        // return entreePanel;   
-        //checkoutPanel.add(createEntreePanel(), "entreeSubMenu");
+    // public JPanel createCheckoutPanel(){
+    //     //Idea is to have 3 submenus that loop back on eachother.
         
-        //Extras SubPanel
-        // JPanel extrasPanel = new JPanel();
-        // JButton hum_fria = new JButton();
-        // JButton dos_falafels = new JButton();
-        // JButton x_protein = new JButton();
-        // JButton x_dressing = new JButton();
-        // extrasPanel.add(hum_fria);
-        // extrasPanel.add(dos_falafels);
-        // extrasPanel.add(x_protein);
-        // extrasPanel.add(x_dressing);
-        // return extrasPanel;
-        //checkoutPanel.add(createExtrasPanel(), "extrasSubMenu");
-
-        //Drinks SubPanel
-        // JPanel drinksPanel = new JPanel();
-        // JButton soft_drink = new JButton();
-        // drinksPanel.add(soft_drink);
-        //checkoutPanel.add(createDrinksPanel(), "drinksSubMenu");
         
-        return checkoutPanel;
+    //     return checkoutPanel;
 
-    }
-    public JPanel createEntreePanel(){
-        JPanel entreePanel = new JPanel();
+    // }
+    // public JPanel createEntreePanel(){
+    //     JPanel entreePanel = new JPanel();
 
-        //Entree Panel Buttons w/ Action Listener. Currently set to go to extras submenu
-        JButton gyroButton = new JButton("Gyro");
-        gyroButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                showCheckoutPanel("extrasSubPanel"); //TODO: Give correct options
-                System.out.println("Gyro Button was pressed.");
-            }
-        });
-        JButton bowlButton = new JButton("Bowl");
-        bowlButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                showCheckoutPanel("extrasSubPanel"); //TODO: Give correct options
-                System.out.println("Bowl Button was pressed.");
-            }
-        });
+    //     //Entree Panel Buttons w/ Action Listener. Currently set to go to extras submenu
+    //     JButton gyroButton = new JButton("Gyro");
+    //     gyroButton.addActionListener(new ActionListener() {
+    //         @Override
+    //         public void actionPerformed(ActionEvent e){
+    //             showCheckoutPanel("extrasSubPanel"); //TODO: Give correct options
+    //             System.out.println("Gyro Button was pressed.");
+    //         }
+    //     });
+    //     JButton bowlButton = new JButton("Bowl");
+    //     bowlButton.addActionListener(new ActionListener() {
+    //         @Override
+    //         public void actionPerformed(ActionEvent e){
+    //             showCheckoutPanel("extrasSubPanel"); //TODO: Give correct options
+    //             System.out.println("Bowl Button was pressed.");
+    //         }
+    //     });
 
-        entreePanel.add(gyroButton);
-        entreePanel.add(bowlButton);
-        return entreePanel;
-    }
-    public JPanel createExtrasPanel(){
-        JPanel extrasPanel = new JPanel();
-        JButton hum_pita= new JButton("Hummas & Pita");
-        JButton dos_falafels = new JButton("2 Falafels");
-        JButton x_protein = new JButton("Extra Protein");
-        JButton x_dressing = new JButton("Extra Dressing");
-        extrasPanel.add(hum_pita);
-        extrasPanel.add(dos_falafels);
-        extrasPanel.add(x_protein);
-        extrasPanel.add(x_dressing);
-        return extrasPanel;
-    }
-    public JPanel createDrinksPanel(){
-        JPanel drinksPanel = new JPanel();
-        JButton soft_drink = new JButton("Soft Drink");
-        drinksPanel.add(soft_drink);
-        return drinksPanel;
-    }
+    //     entreePanel.add(gyroButton);
+    //     entreePanel.add(bowlButton);
+    //     return entreePanel;
+    // }
+    // public JPanel createExtrasPanel(){
+    //     JPanel extrasPanel = new JPanel();
+    //     JButton hum_pita= new JButton("Hummas & Pita");
+    //     JButton dos_falafels = new JButton("2 Falafels");
+    //     JButton x_protein = new JButton("Extra Protein");
+    //     JButton x_dressing = new JButton("Extra Dressing");
+    //     extrasPanel.add(hum_pita);
+    //     extrasPanel.add(dos_falafels);
+    //     extrasPanel.add(x_protein);
+    //     extrasPanel.add(x_dressing);
+    //     return extrasPanel;
+    // }
+    // public JPanel createDrinksPanel(){
+    //     JPanel drinksPanel = new JPanel();
+    //     JButton soft_drink = new JButton("Soft Drink");
+    //     drinksPanel.add(soft_drink);
+    //     return drinksPanel;
+    // }
 
 }
