94a45c74fd246812e4a2e093b31be870a0e50f53
Sonofdantu <106547830+Sonofdantu@users.noreply.github.com>
2023-03-06 16:23:32 -0600

    Add files via upload

---
+++ project-2-epsilon-Cameron-Yoffe/com.p_h_gui/src/App.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+import java.util.Scanner;
+import java.io.*;
+import javax.swing.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        new Login();
+    }
+}
---
+++ project-2-epsilon-Cameron-Yoffe/com.p_h_gui/src/GUI.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.BorderFactory;
+// import javax.swing.border.Border;
+
+
+
+public class GUI extends JFrame{
+    // private JFrame frame;
+    private MainPanel mainPanel;
+    private NavPanel navPanel;
+   
+    public GUI(){
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c = setGridBagConstraints(c);
+
+        navPanel = new NavPanel();
+        navPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        add(navPanel, c);
+
+        mainPanel = new MainPanel(); // This panel SHOULD CHANGE based on what is needed by the user.
+        mainPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        c.gridx = 1;
+        c.gridy = 0;
+        c. gridwidth = 2;
+        add(mainPanel, c);
+
+
+        setGUIFrame();
+        
+    } // Contructor
+    private GridBagConstraints setGridBagConstraints(GridBagConstraints c){
+        c.fill = GridBagConstraints.VERTICAL;
+        c.weightx = 1/6.0;
+        c.gridx = 0; // Position the first button at column 0
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = GridBagConstraints.REMAINDER;
+
+        return c;
+    } // Moved constraints to clean code
+
+    private void setGUIFrame(){
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1200, 800);
+        setVisible(true);
+    } // Cleaning up GUI Constructor by moving some of the frame creation stuff outside of the constructor
+
+}
